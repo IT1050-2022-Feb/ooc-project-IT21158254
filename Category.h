20ab4ed1e23e313ceb3f662766a32db4181304a0
IT21158254 <>
2022-05-20 22:51:43 +0530

    resolved errors

--- Category.h
+++ Category.h
@@ -1,19 +1,17 @@
-#include "Product.h" 
+#include "Product.h"
 #define SIZE1 1;
-class Category{
-	 private:
-	 	int categoryID;
-	 	string categoryName;
-	 	string description;
-	 	Product *product[SIZE1];
-	 	
-	public:
-		void getProductsInCategory();
-		void DisplayCategoryDetails();
-		Category();
-		Category(int categoryid,int pid);
+class Category
+{
+private:
+	int categoryID;
+	string categoryName;
+	string description;
+	Product *product[SIZE1];
 
-	
-	
-	
+	// res
+public:
+	void getProductsInCategory();
+	void DisplayCategoryDetails();
+	Category();
+	Category(int categoryid, int pid);
 };
