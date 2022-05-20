d44bd95289c7ebe76ad504b4707c4649e1bf71db
IT21158322 <99183621+IT21158322@users.noreply.github.com>
2022-05-20 14:54:46 +0000

    Product/ Category/ Main

---
+++ DeliveryDetails.cpp
@@ -0,0 +1,20 @@
+#inlcude<iostream>
+#include<cstring>
+#inlcude "DeliveryDetails.h"
+using namespace std;
+
+DeliveryDetails::DeliveryDetails(){
+	
+	deliveryID=0;
+}
+
+DeliveryDetails::DeliveryDetails(int deliveryid){
+	
+   deliveryID=deliveryid;
+}
+
+DeliveryDetails::~DeliveryDetails(){
+	
+	cout<<"Delivery Details ID:"<<deliveryID<<endl;
+}
+
---
+++ DeliveryDetails.h
@@ -0,0 +1,13 @@
+class DeliveryDetails{
+private:	
+	int deliveryID;
+public:
+	DeliveryDetails();
+	DeliveryDetails(int deliveryid);
+	void updateDeliveryStatus();
+	void displayDeliveryDetails();
+	~DeliveryDetails();
+	
+	
+	
+};
